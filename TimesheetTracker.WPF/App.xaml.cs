3eb48587a2ef9b1d1ebc265d069e84819a1908a1
captainsalt <dontbothertheking@gmail.com>
2026-01-10 11:26:54 -0600

    Add new Timesheet project

--- TimesheetTracker.WPF/App.xaml.cs
+++ TimesheetTracker.WPF/App.xaml.cs
@@ -2,12 +2,11 @@
 using System.Data;
 using System.Windows;
 
-namespace TimesheetTracker
+namespace TimesheetTracker.WPF;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
 }
+
--- TimesheetTracker.WPF/MainWindow.xaml.cs
+++ TimesheetTracker.WPF/MainWindow.xaml.cs
@@ -9,22 +9,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TimesheetTracker
+namespace TimesheetTracker.WPF;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Views.ProjectAdder projectAdder = new Views.ProjectAdder();
-            projectAdder.ShowDialog();
-        }
+        InitializeComponent();
     }
 }
